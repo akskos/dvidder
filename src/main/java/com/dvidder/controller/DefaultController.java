afd2e048ffc1218a3a4d270243ce8ecd3c54068c
Akseli Koskinen <koskinen.akseli@gmail.com>
2016-12-17 22:27:07 +0200

    Add post entity and repository and handle requests to create new posts

--- src/main/java/com/dvidder/controller/DefaultController.java
+++ src/main/java/com/dvidder/controller/DefaultController.java
@@ -6,11 +6,17 @@
 package com.dvidder.controller;
 
 import com.dvidder.domain.Account;
+import com.dvidder.domain.Post;
+import com.dvidder.repository.AccountRepository;
+import com.dvidder.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -19,11 +25,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class DefaultController {
+    
+    @Autowired
+    AccountRepository accountRepository;
+    
+    @Autowired
+    PostRepository postRepository;
 
     @RequestMapping("/")
     public String index(Model model) {
         User currentUser = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("username", currentUser.getUsername());
         return "index";
+    }
+    
+    // Create a post
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public String post(@RequestParam String content) {
+        
+        // Find current account
+        User currentUser = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Account currentAccount = accountRepository.findByUsername(currentUser.getUsername());
+        
+        // Create the post
+        Post post = new Post();
+        post.setContent(content);
+        post.setOwner(currentAccount);
+        postRepository.save(post);
+        
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/dvidder/domain/Post.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dvidder.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author akseli
+ */
+@Entity
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long postId;
+    
+    @ManyToOne
+    Account owner;
+
+    private String content;
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public Account getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Account owner) {
+        this.owner = owner;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
