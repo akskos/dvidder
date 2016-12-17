78430e331e1b88b3019b86a0aa6cfb454f8d5a0f
Akseli Koskinen <koskinen.akseli@gmail.com>
2016-12-17 19:53:42 +0200

    Add production profile and DevInitializer

---
+++ src/main/java/com/dvidder/config/DevInitializer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dvidder.config;
+
+import com.dvidder.domain.Account;
+import com.dvidder.repository.AccountRepository;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author akseli
+ */
+@Component
+public class DevInitializer {
+    
+    @Autowired
+    AccountRepository accountRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @PostConstruct
+    public void init() {
+        Account user1 = new Account();
+        user1.setUsername("akseli");
+        user1.setPassword(passwordEncoder.encode("1234"));
+        accountRepository.save(user1);
+    }
+}
