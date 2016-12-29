33e44914f28b4397a7fffd3a5dac1b0e3039d072
Akseli Koskinen <koskinen.akseli@gmail.com>
2016-12-29 17:06:50 +0200

    Add test for liking a post

--- src/test/java/com/dvidder/controller/ReactionTest.java
+++ src/test/java/com/dvidder/controller/ReactionTest.java
@@ -10,11 +10,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  *
@@ -36,7 +43,11 @@ public class ReactionTest {
     }
     
     @Test
-    public void likePost() {
-        
+    @WithUserDetails("test_user")
+    public void likePost() throws Exception {
+        mockMvc.perform(post("/posts/1/like"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.likers[0]").value("test_user"));
     }
 }
